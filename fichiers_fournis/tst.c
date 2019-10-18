c29097c8295c8b547ba01ad3b083541a22e6496c
Ali El Mufti <alielmufti25@gmail.com>
2019-10-18 15:17:54 +0200

    Test des commande

--- fichiers_fournis/tst.c
+++ fichiers_fournis/tst.c
@@ -4,6 +4,16 @@
 
 #include <stdio.h>
 #include "readcmd.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <limits.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
 
 
 int main()
@@ -33,6 +43,14 @@ int main()
 		/* Display each command of the pipe */
 		for (i=0; l->seq[i]!=0; i++) {
 			char **cmd = l->seq[i];
+			pid_t p = fork();
+			if(p ==0)
+			execvp(cmd[0], cmd);
+			else{
+				int status;
+				waitpid(p, &status, 0 );
+			}
+		
 			printf("seq[%d]: ", i);
 			for (j=0; cmd[j]!=0; j++) {
 				printf("%s ", cmd[j]);
