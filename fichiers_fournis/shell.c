39cfe5f70b6f6adf65f6f405d1e8f4f6e0e814b6
William Nelson <swilliam.nelson@gmail.com>
2019-10-18 15:46:27 +0200

    salut

---
+++ fichiers_fournis/shell.c
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2002, Simon Nieuviarts
+ */
+
+#include <stdio.h>
+#include "readcmd.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <limits.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+
+
+int main()
+{
+	while (1) {
+		struct cmdline *l;
+		int i, j;
+
+		printf("shell> ");
+		l = readcmd();
+
+		/* If input stream closed, normal termination */
+		if (!l) {
+			printf("exit\n");
+			exit(0);
+		}
+
+		if (l->err) {
+			/* Syntax error, read another command */
+			printf("error: %s\n", l->err);
+			continue;
+		}
+
+		if (l->in) printf("in: %s\n", l->in);
+		if (l->out) printf("out: %s\n", l->out);
+
+		/* Display each command of the pipe */
+		for (i=0; l->seq[i]!=0; i++) {
+			char **cmd = l->seq[i];
+			pid_t p = fork();
+			if(p ==0)
+			execvp(cmd[0], cmd);
+			else{
+				int status;
+				waitpid(p, &status, 0 );
+			}
+
+			printf("seq[%d]: ", i);
+			for (j=0; cmd[j]!=0; j++) {
+				printf("%s ", cmd[j]);
+			}
+			printf("\n");
+		}
+	}
+}
